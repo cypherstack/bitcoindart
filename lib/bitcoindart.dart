3ec0912a3bcfeaef681fd93c12dda4617c23d804
julian <julian@cypherstack.com>
2022-05-10 14:17:01 -0600

    expose p2sh

--- lib/bitcoindart.dart
+++ lib/bitcoindart.dart
@@ -3,13 +3,14 @@
 /// More dartdocs go here.
 library bitcoindart;
 
-export 'src/bitcoindart_base.dart';
-export 'src/models/networks.dart';
-export 'src/transaction.dart';
 export 'src/address.dart';
-export 'src/transaction_builder.dart';
+export 'src/bitcoindart_base.dart';
 export 'src/ecpair.dart';
-export 'src/payments/p2pkh.dart';
-export 'src/payments/p2wpkh.dart';
+export 'src/models/networks.dart';
 export 'src/payments/index.dart';
+export 'src/payments/p2pkh.dart';
+export 'src/payments/p2sh.dart';
+export 'src/payments/p2wpkh.dart';
+export 'src/transaction.dart';
+export 'src/transaction_builder.dart';
 // TODO: Export any libraries intended for clients of this package.
